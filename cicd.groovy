c00fe0e5ba09b11cfc24375c9d250690d5cda1c9
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-24 13:29:08 -0400

    add polling to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,14 @@
 node('linux')
 {
+   
+  stage (Poll) {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/powerline-goport.git"]]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/powerline-goport.git'), string(name: 'PORT_DESCRIPTION', value: 'A beautiful and useful low-latency prompt for your shell, written in go' )]
   }
