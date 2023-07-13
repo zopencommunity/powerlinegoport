189e913f097e6a10d426203b72110148c5ca9d18
Igor Todorovski <itodorov@ca.ibm.com>
2023-07-13 12:55:46 -0400

    Update powerline go so that we can generate a github release

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/powerline-goport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/powerlinegoport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/powerline-goport.git'), string(name: 'PORT_DESCRIPTION', value: 'A beautiful and useful low-latency prompt for your shell, written in go' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/powerlinegoport.git'), string(name: 'PORT_DESCRIPTION', value: 'A beautiful and useful low-latency prompt for your shell, written in go' )]
   }
 }
