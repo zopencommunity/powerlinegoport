348fb4dda71009ed2fa277a009d024aec3ac3314
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:36:31 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/powerlinegoport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/powerlinegoport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/powerlinegoport.git'), string(name: 'PORT_DESCRIPTION', value: 'powerline-go' ), string(name: 'BUILD_LINE', value: 'DEV'), string(name: 'NODE_LABEL', value: "go_120" ) ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/powerlinegoport.git'), string(name: 'PORT_DESCRIPTION', value: 'powerline-go' ), string(name: 'BUILD_LINE', value: 'DEV'), string(name: 'NODE_LABEL', value: "go_120" ) ]
   }
 }
